b2007de47e697330c2f4563b1be48119d216e3e9
deen992 <88670981+deen992@users.noreply.github.com>
2025-07-10 14:23:21 +0800

    Update DBConnection.java

--- src/main/java/com/hms/db/DBConnection.java
+++ src/main/java/com/hms/db/DBConnection.java
@@ -4,24 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+    private static Connection conn;
 
-	private static Connection conn;
-	
-	public static Connection getConn() {
-		
-		try {
-			
-			//step:1 for connection - load the driver class 
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//step:2- create a connection
-			conn = DriverManager.getConnection("jdbc:mysql://sql305.infinityfree.com:3306/if0_39332728_hospital","if0_39332728","1y3p4PZxbYTP");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-		
-		return conn;
-	}
+    public static Connection getConn() {
+        if (conn == null) {
+            try {
+                // 1) load the driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                // 2) read connection info from env-vars
+                String url  = System.getenv("jdbc:mysql://sql5.freesqldatabase.com:3306/sql5789259?useSSL=false&serverTimezone=UTC");
+                String user = System.getenv("sql5789259");
+                String pass = System.getenv("RKwW7AUxsf");
+
+                conn = DriverManager.getConnection(url, user, pass);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
 }
